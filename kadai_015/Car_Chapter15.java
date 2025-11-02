5858013d2fa53dbdedeaae360c689ab1d01fc706
Anney-Hub <yunanoheya91@gmail.com>
2025-11-03 08:30:23 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,39 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+		
+		private int gear = 1;
+		private int speed = 10;
+		
+//		public Car_Chapter15( int gear, int speed) {
+//			this.gear = gear;
+//			this.speed = speed;
+//			
+//			System.out.println("最初のギアは" + this.gear + "です");
+//			System.out.pritnln("最初の速度は" + this.speed + "です");
+//		}
+		
+		
+		public void changeGear( int afterGear ) {
+			
+			this.gear = afterGear;
+			
+			System.out.println("ギアを" + this.gear + "から" + afterGear + "に切り替えました");
+			
+			
+		}
+		
+		public void run() {
+
+			this.speed = switch( this.gear ) {
+				case 1 -> 10;
+				case 2 -> 20;
+				case 3 -> 30;
+				case 4 -> 40;
+				case 5 -> 50;
+				default -> 10;
+			};
+			System.out.println("速度は" + this.speed + "kmです");
+		}
+		
+	}
